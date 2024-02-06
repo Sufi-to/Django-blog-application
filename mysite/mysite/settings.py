601e59d65a475b20810d3fd77c5787ac60c6bb43
Sufi-to <3thomas225@gmail.com>
2024-02-07 01:18:56 +0400

    fixed cred

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -130,5 +130,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tomastekle20@gmail.com'
-EMAIL_HOST_PASSWORD = 'aknq cajl insa cyfe '
+
